ebb5d687eea7a5832f58faf3a245e36959a222f7
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-10-21 20:45:39 +0100

    Began Turret Script

---
+++ Game-Engines-DT508-3/Assets/turretControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretControl : MonoBehaviour
+{
+    //public GameObject target;
+    //public GameObject turretPivot;
+
+    public Transform currentPosition;
+    public Transform targetPosition;
+
+    private float timeCount = 0.0f;
+    
+    void Start()
+    {
+        //currentPosition = turretPivot.transform;
+    }
+
+    
+    void Update()
+    {
+        //targetPosition = target.transform;
+        transform.rotation = Quaternion.Slerp(currentPosition.rotation, targetPosition.rotation, timeCount);
+        timeCount = timeCount + Time.deltaTime;
+    }
+}
