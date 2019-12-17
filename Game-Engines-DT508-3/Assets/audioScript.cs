08b17a0916779ed4a52d57c813ca116217354dcb
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-12-17 15:22:15 +0000

    audioScript created
    Made an audio script which uses GetSpectrumData to output 512 samples from a specified audiosource.

---
+++ Game-Engines-DT508-3/Assets/audioScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class audioScript : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public float[] samples = new float[512];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
