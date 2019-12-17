7cf310cce80c4f20e6ae4fc8e870831839dc835c
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-12-17 15:45:22 +0000

    instantiateAudioSpectrum script created
    added a script which instantiates 512 prefabs for the ausio samples in the audioscript to affect in real-time via whatver music file is being played throufh the audiosource.

--- Game-Engines-DT508-3/Assets/audioScript.cs
+++ Game-Engines-DT508-3/Assets/audioScript.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class audioScript : MonoBehaviour
 {
     private AudioSource audioSource;
-    public float[] samples = new float[512];
+    public static float[] samples = new float[512];
     
     // Start is called before the first frame update
     void Start()
---
+++ Game-Engines-DT508-3/Assets/instantiateAudioSpectrum.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateAudioSpectrum : MonoBehaviour
+{
+    public GameObject audioSamplePrefab;
+    GameObject[] sampleObject = new GameObject[512];
+    public float maxScale;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject sampleObjectInstance = Instantiate(audioSamplePrefab);
+            sampleObjectInstance.transform.position = transform.position;
+            sampleObjectInstance.transform.parent = transform;
+            sampleObjectInstance.name = "Sample Object #" + i;
+            transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            sampleObjectInstance.transform.position = Vector3.forward * 100;
+            sampleObject[i] = sampleObjectInstance;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (sampleObject != null)
+            {
+                sampleObject[i].transform.localScale = new Vector3(1, (audioScript.samples[i] * maxScale) + 2, 1);
+            }
+        }
+    }
+}
